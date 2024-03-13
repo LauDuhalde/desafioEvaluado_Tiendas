88e2f8e176d0b72c801190b9d0510cabce156386
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2024-03-13 18:02:40 -0300

    avance

--- tienda.py
+++ tienda.py
@@ -1,12 +1,22 @@
 from abc import ABC, abstractmethod
+from producto import Producto
 
 class Tienda(ABC):
     def __init__(self, nombre, costo_delivery):
         self.__nombre = nombre
         self.__costo_delivery = costo_delivery
         self.__productos = []
-
-    def ingresar_producto(self, producto):
+        
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @property
+    def costo_delivery(self):
+        return self.__costo_delivery
+        
+    def ingresar_producto(self, nombre, stock):
+        producto = Producto(nombre, stock)
         for p in self.__productos:
             if p == producto:
                 producto = producto + p
